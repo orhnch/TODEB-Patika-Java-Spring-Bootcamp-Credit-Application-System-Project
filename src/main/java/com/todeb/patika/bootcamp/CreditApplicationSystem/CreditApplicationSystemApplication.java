0ac7ceaadd6f5cbfc30c511cbfcc6d2dcdc93c8c
orhnck <orhan.cakmak222@gmail.com>
2022-07-28 13:08:02 +0300

    project was constructed

---
+++ src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/CreditApplicationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.todeb.patika.bootcamp.CreditApplicationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApplicationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditApplicationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todeb/patika/bootcamp/CreditApplicationSystem/CreditApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todeb.patika.bootcamp.CreditApplicationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
