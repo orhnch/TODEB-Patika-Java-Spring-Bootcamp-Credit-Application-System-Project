e7b654e556dbc3bce196f36ead4116fb970c8f4f
orhnck <orhan.cakmak222@gmail.com>
2022-07-29 16:35:59 +0300

    crud operations were added

--- src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/controller/CreditController.java
+++ src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/controller/CreditController.java
@@ -1,4 +1,60 @@
 package com.todeb.patika.bootcamp.CreditApplicationSystem.controller;
 
+import com.todeb.patika.bootcamp.CreditApplicationSystem.model.dto.CreditDTO;
+import com.todeb.patika.bootcamp.CreditApplicationSystem.model.entity.Credit;
+import com.todeb.patika.bootcamp.CreditApplicationSystem.service.CreditService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/credit")
 public class CreditController {
+    @Autowired
+    private CreditService creditService;
+
+    @GetMapping("/all")
+    public ResponseEntity getAllCredits() {
+        List<Credit> allCredits = creditService.getAllCredits();
+        return ResponseEntity.ok(allCredits);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getCreditById(@PathVariable Long id) {
+        Credit creditById = creditService.getCreditById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(creditById);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity createNewCredit(@RequestBody CreditDTO credit) {
+        Credit respCredit = creditService.create(credit);
+        if (respCredit == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Credit could not be created successfully");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(respCredit);
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteCredit(@RequestParam(name = "id") Long id) {
+        creditService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Related Credit deleted successfully");
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity updateCredit(
+            @PathVariable Long id,
+            @RequestBody CreditDTO customer) {
+        Credit update = creditService.update(id, customer);
+        return ResponseEntity.status(HttpStatus.OK).body(update);
+    }
+
+    @DeleteMapping("delete/all")
+    public ResponseEntity deleteAllCredits() {
+        creditService.deleteAll();
+        return ResponseEntity.status(HttpStatus.OK).body("All credits were deleted successfully");
+    }
 }
--- src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/service/CreditService.java
+++ src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/service/CreditService.java
@@ -1,11 +1,50 @@
 package com.todeb.patika.bootcamp.CreditApplicationSystem.service;
 
+import com.todeb.patika.bootcamp.CreditApplicationSystem.exception.EntityNotFoundException;
+import com.todeb.patika.bootcamp.CreditApplicationSystem.model.dto.CreditDTO;
+import com.todeb.patika.bootcamp.CreditApplicationSystem.model.entity.Credit;
+import com.todeb.patika.bootcamp.CreditApplicationSystem.model.mapper.CreditMapper;
 import com.todeb.patika.bootcamp.CreditApplicationSystem.repository.CreditRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CreditService {
     private final CreditRepository creditRepository;
+
+    public List<Credit> getAllCredits() {
+        List<Credit> allCredits = creditRepository.findAll();
+        return allCredits;
+    }
+
+    public Credit create(CreditDTO creditDTO) {
+        Credit credit = CreditMapper.toEntity(creditDTO);
+        return creditRepository.save(credit);
+    }
+
+    public Credit getCreditById(Long id) {
+        Optional<Credit> byId = creditRepository.findById(id);
+        return byId.orElseThrow(() -> new EntityNotFoundException("Credit", "id: " + id));
+    }
+
+    public void delete(Long id) {
+        getCreditById(id);
+        creditRepository.deleteById(id);
+    }
+
+    public Credit update(Long id, CreditDTO credit) {
+        Credit creditById = getCreditById(id);
+        creditById.setCreditLimit(credit.getCreditLimit());
+        creditById.setStatus(credit.getStatus());
+
+        return creditRepository.save(creditById);
+    }
+
+    public void deleteAll() {
+        creditRepository.deleteAll();
+    }
 }
