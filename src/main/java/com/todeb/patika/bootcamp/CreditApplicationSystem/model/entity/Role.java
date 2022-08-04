4e8ab54e378575fc62e9c67dee5bb96c6afea9c1
orhnck <orhan.cakmak222@gmail.com>
2022-08-04 18:09:05 +0300

    user class and role enum was added

---
+++ src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/model/entity/Role.java
@@ -0,0 +1,11 @@
+package com.todeb.patika.bootcamp.CreditApplicationSystem.model.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_ADMIN, ROLE_CLIENT;
+
+    public String getAuthority() {
+        return name();
+    }
+}
---
+++ src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/model/entity/User.java
@@ -0,0 +1,40 @@
+package com.todeb.patika.bootcamp.CreditApplicationSystem.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(min = 5, max = 25, message = "username length should be between 5 and 25 characters")
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Size(min = 5, message = "Minimum password length: 5 characters")
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<Role> roles;
+}
+
