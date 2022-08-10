02906e928d0d76a44f68a29402356ce24a2fb519
orhnck <orhan.cakmak222@gmail.com>
2022-08-10 18:20:32 +0300

    SmsControllerTest and SmsServiceTest were added

---
+++ src/test/java/com/todeb/patika/bootcamp/CreditApplicationSystem/controller/SmsControllerTest.java
@@ -0,0 +1,12 @@
+package com.todeb.patika.bootcamp.CreditApplicationSystem.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmsControllerTest {
+
+    @Test
+    void sendSMS() {
+    }
+}
---
+++ src/test/java/com/todeb/patika/bootcamp/CreditApplicationSystem/service/SmsServiceTest.java
@@ -0,0 +1,16 @@
+package com.todeb.patika.bootcamp.CreditApplicationSystem.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmsServiceTest {
+
+    @Test
+    void sendSMS() {
+    }
+
+    @Test
+    void save() {
+    }
+}
