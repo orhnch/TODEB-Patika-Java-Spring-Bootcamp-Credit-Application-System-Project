cf8904a5210a9ac46063c9f8fb73509bc596b122
orhnck <orhan.cakmak222@gmail.com>
2022-08-12 14:24:54 +0300

    swagger connection was done

--- src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/CreditApplicationSystemApplication.java
+++ src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/CreditApplicationSystemApplication.java
@@ -2,7 +2,9 @@ package com.todeb.patika.bootcamp.CreditApplicationSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class CreditApplicationSystemApplication {
 
--- src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/config/WebSecurityConfig.java
+++ src/main/java/com/todeb/patika/bootcamp/CreditApplicationSystem/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -39,6 +40,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // Apply JWT
         http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
 
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+//                swagger2 base urls
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**",
+//                swagger3 base urls
+                "/swagger-ui/**",
+                "/javainuse-openapi/**");
     }
 
     @Bean
